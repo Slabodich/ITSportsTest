590e1990c6bd73005e422cefb183dd0fb39e879b
John Doe <johndoe@example.com>
2023-06-04 14:34:58 +0300

    add second task

--- main.ts
+++ main.ts
@@ -5,5 +5,32 @@ interface ResData {
   body: string;
 }
 
+class FifteenthTitle {
+  static #URL = "https://jsonplaceholder.typicode.com/posts";
+  static #ID = 15;
+
+  static #get() {
+    fetch(this.#URL)
+      .then((res) => {
+        if (res.ok) {
+          return res.json(); // Добавлен return
+        }
+      })
+      .then((resData) => this.#getTitleFromJson(resData))
+      .then((title) => console.log(title))
+      .catch(({ message }) => console.error(message));
+  }
+
+  static #getTitleFromJson(resData: ResData[]) {
+    const itemWithTitle = resData.find((item) => item.id === this.#ID); // Добавлен вызов метода find()
+    const title = itemWithTitle?.title; // Добавлен оператор безопасного доступа '?'
+    return title; // Добавлен return
+  }
+
+  run() {
+    FifteenthTitle.#get();
+  }
+}
+
 const fifteenthTitle = new FifteenthTitle();
 fifteenthTitle.run();
